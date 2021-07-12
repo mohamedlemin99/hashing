0384c8903305b6863773fcb5f4fb510274de530f
lemin med <mohamed96lemin@gmail.com>
2021-07-12 23:48:43 +0200

    open addressing

--- main.c
+++ main.c
@@ -1,157 +1,188 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 #define max 7
 clock_t start,end;
+
 struct student{int day,month,year;
 int ID;
  char name[100];
  int score_last_year;
 };
-struct hashing{
-struct student *birthday;
+struct node{
+struct student data;
+struct student *next;
 };
-struct hashing *sarray;
-int linear_probing(int tot)
-{
-    for(int i=0; i<max; i++)
- {int key= ((tot+i)%max);
+struct openhashing{
+struct node *usename;
 
-    if(sarray[key].birthday==NULL)
-    return key;
- }
- printf("Collision element already exist!\n");
-        printf("Unable to insert \n");
-        return -1;
-}
 
-int quadratic_probing(int tot)
-{
-   for(int i=0; i<max; i++)
- {int key= ((tot+i*i)%max);
-
-    if(sarray[key].birthday==NULL)
-    return key;
- }
-printf("Collision element already exist!\n");
-        printf("Unable to insert \n");
-        return -1;
-}
-void init_array()
+};
+struct openhashing *narray;
+void init()
 {
     int i;
-    for (i = 0; i < max; i++)
-    {
-	sarray[i].birthday = NULL;
-	//sarray[i].birthday->day=0,sarray[i].birthday->month=0,sarray[i].birthday->year=0;
-
-    }
+    for(i = 0; i < max; i++)
+        narray[i].usename = NULL;
 }
-void insert_student()
+void insertop()
 {
-    int key,num;
-        struct student *new_item = (struct student*) malloc(sizeof(struct student));
-
-    printf("\tName:\n");
-     fflush(stdin);
-     fgets(new_item->name, sizeof(new_item->name), stdin);  // read name
-     printf("\tEnter the student ID\n");
-     scanf("%d",&new_item->ID);
-     printf("\tEnter the student score_last_year\n");
-     scanf("%d",&new_item->score_last_year);
-     printf("\n enter the birth date\n");
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
+    if(newNode==NULL)printf("Unable to allocate memory.");
+    else{   printf("\n Name: ");
+    fflush(stdin);
+    fgets(newNode->data.name, sizeof(newNode->data.name), stdin);
+   printf("\n enter the student ID ");
+    scanf("%d",&newNode->data.ID);
+    printf("\n enter the student score of last year ");
+    scanf("%d",&newNode->data.score_last_year);
+    printf("\n enter the birth date\n");
     printf(" year :\t");
-    scanf("%d",&new_item->year);
-    printf("\n month :\t");
-    scanf("%d",&new_item->month);
+    scanf("%d",&newNode->data.year);
     printf("\n day :\t");
-    scanf("%d",&new_item->day);
-
-
-    int tot=new_item->year*360+new_item->month*30+new_item->day;
-    printf("enter 1 to do linear probing \n enter 2 to do quadratic probing \t");
-    scanf("%d",&num);
+    scanf("%d",&newNode->data.day);
+    printf("\n month :\t");
+    scanf("%d",&newNode->data.month);
+    newNode->next=NULL;
+    int tot=getasci(newNode->data.name);
+    int key=(tot%max);
     start=clock();
-    switch(num){
-   case(2):key=quadratic_probing(tot);
-   break;
-   case(1):key=linear_probing(tot);
-   break;
-    }
-    if(key!=-1)
-         sarray[key].birthday=new_item;
-}
-int search(int nday, int nmonth,int nyear,char Name[100])
-{
-    int tot=nyear*360+nmonth*30+nday;
 
-    for(int i=0; i<max; i++){
-        int key = (tot+i) % max;
-    if(sarray[key].birthday!=NULL)
+    if(narray[key].usename==NULL)
     {
-        if((sarray[key].birthday->day==nday)&&(sarray[key].birthday->month==nmonth)&&(sarray[key].birthday->year==nyear)/*&&(strcmp(sarray[key].birthday->name,Name)==0)*/)
+     narray[key].usename=newNode;
+     printf("not used\n");
+    }
+     else
+    {
+        //add the node at the end of chain[key].
+        struct node *temp = narray[key].usename;
+        while(temp->next)
         {
-        return key;}
+            temp = temp->next;
+        }
 
-    }}
-    return -1;
-
+        temp->next = newNode;
+    }
+    }
 }
-void del(int nday, int nmonth,int nyear,char Name[100])
+int getasci(char name[100])
 {
-    int key=search(nday,nmonth,nyear,Name);
-    if(key != -1){sarray[key].birthday=NULL;
-    printf("deleted successfully");}
-    else
-        printf("not present in the hash table\n");
+    int test[100]={},i=0,j,tot=0;
+while(name[i]!='\0') {test[i]=name[i]; i++;}
+for(j=0;j<i-1;j++)
+{
+tot=tot+(int)(test[j]);
 }
-void display(){
+return tot;
+}
+void displayy()
+{
 for(int i=0;i<max; i++)
     {
-         if(sarray[i].birthday!=NULL)
-        printf("index %d name: %s\t score_last_year: %d \t ID: %d  birthday %d/%d/%d\n",i ,sarray[i].birthday->name,sarray[i].birthday->score_last_year,sarray[i].birthday->ID,sarray[i].birthday->day,sarray[i].birthday->month,sarray[i].birthday->year);
+         if(narray[i].usename!=NULL){
+                struct node *temp = narray[i].usename;
+        while(temp)
+        {
+            printf("index: %d",i);
+            printf(" name: %s\t score_last_year: %d \t ID: %d  birthday %d/%d/%d",temp->data.name,temp->data.score_last_year,temp->data.ID,temp->data.day,temp->data.month,temp->data.year);
+            temp = temp->next;
+            printf("\t-->\t");
+        }
+        printf("\n");
+
+        }
         else printf("index %d empty place\n",i);
 
-    }}
-    void closed_addressing()
+    }
+}
+deleet()
+{
+   char nname[100];int id;
+    printf("\n Name: to delete \n ");
+    fflush(stdin);
+    fgets(nname, sizeof(nname), stdin);
+    printf("\n enter the student ID ");
+    scanf("%d",&id);
+    int tot=getasci(nname);
+    int key=(tot%max);
+    struct node *head = narray[key].usename;
+    struct node *temp=head->next;
+    struct node *deleted;
+    if((strcmp(head->data.name,nname)==0)&&(id==head->data.ID))
     {
-        int x;
-
-    sarray = (struct hashing*) malloc(max*sizeof(struct hashing*));
-    printf("1Hello world!\n");
-    init_array();
-    printf("the memory requried to store the data for closed addressing is %d\n",sizeof(struct hashing));
-    printf("the memory requried to store the data for student %d\n",sizeof(struct student));
-    b:
-    printf("enter\n 1 to insert\n 2 to delete student\n 3 to search\n 4 to display\n");
-    scanf("%d",&x);
-switch(x)
+      narray[key].usename=head->next;
+      free(head);
+      return;
+    }
+    while(temp){
+    if((strcmp(temp->data.name,nname)==0)&&(id==head->data.ID))
     {
-    case(1):
-        insert_student();
-        end=clock();
-    break;
-    case(3):{int key=search(14, 12,1999,"ahmed");
-    if(key!=-1)
-    printf("Search Found found at index: %d\n",key);
-    else printf("Search Not Found\n");
-    break;}
-    case(2):del(14, 12,1999,"ahmed");break;
-
-   case(4):{
-    display();break;}
-   default:printf("out of range\n");break;}
-
-   double cpu_time_used=((double)(end-start))/ CLOCKS_PER_SEC ;
-printf(" time taken by function is %lf\n",cpu_time_used);
-    goto b;
+        deleted=temp;
+        temp=temp->next;
+        free(deleted);
+       return;
+    }
+    temp=temp->next;
 
     }
+    printf("%s    not found\n",nname);
+}
+void searchh()
+{
+    char nname[100];int id;
+    printf("\n Name: to seach for\n ");
+    fflush(stdin);
+    fgets(nname, sizeof(nname), stdin);
+     printf("\n enter the student ID ");
+    scanf("%d",&id);
+    int tot=getasci(nname);
+    int key=(tot%max);
+    struct node *temp = narray[key].usename;
+    while(temp){
+    if((strcmp(temp->data.name,nname)==0)&&(temp->data.ID==id))
+    {
+        printf("index: %d",key);
+            printf(" name: %s\t score_last_year: %d \t ID: %d  birthday %d/%d/%d",temp->data.name,temp->data.score_last_year,temp->data.ID,temp->data.day,temp->data.month,temp->data.year);
+return;
+    }
+    temp=temp->next;
+
+    }
+    printf("%s    not found\n",nname);
+
+
+}
+void open_adressing()
+{
+    narray = (struct openhashing*) malloc(max*sizeof(struct openhashing*));
+    init();
+    int num;
+    printf("the memory requried to store the data for open addressing is %d\n",sizeof(struct openhashing));
+    printf("the memory requried to store the data for student %d\n",sizeof(struct student));
+    b:
+
+    printf("enter\n 1 to insert\n 2 to display \n 3 to search \n 4 to delete !\n");
+    scanf("%d",&num);
+    switch(num)
+    {
+        case(1):
+            insertop();
+            end=clock();
+            break;
+        case(2):displayy();break;
+        case(3):searchh();break;
+        case(4):deleet();break;
+        default:printf("out of range\n");break;
+    }
+    double cpu_time_used=((double)(end-start))/ CLOCKS_PER_SEC ;
+printf(" time taken by function is %lf\n",cpu_time_used);
+    goto b;
+}
 int main()
 {
     printf("welcome doctor omar\n");
-    closed_addressing();
-
+    open_adressing();
     return 0;
 }
