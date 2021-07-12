d349e1899131c3ab9a1f38c4bf18e92552e3cf3b
lemin med <mohamed96lemin@gmail.com>
2021-07-12 23:35:14 +0200

    main function

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define max 7
+clock_t start,end;
+struct student{int day,month,year;
+int ID;
+ char name[100];
+ int score_last_year;
+};
+
+int main()
+{
+    printf("welcome doctor omar\n");
+
+
+    return 0;
+}
